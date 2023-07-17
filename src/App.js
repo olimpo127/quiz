46dfb64eb3631d8a5cf17ea5fb2f4f916c882a58
Felipe Ojeda <fojedai47@gmail.com>
2023-07-17 14:16:15 -0400

    v 1.0

--- src/App.js
+++ src/App.js
@@ -1,65 +1,91 @@
-import React, { useState } from 'react';
-import './App.css'; // Import the CSS file for styling
+import React, { useState } from "react";
+import "./App.css"; // Import the CSS file for styling
 
 const Quiz = () => {
   const questions = [
     {
-      question: 'Who defeated Luffy first?',
-      options: ['Portgas D. Ace', 'Crocodile', 'Smoker', 'Rob Lucci'],
-      answer: 0 // Index of the correct option
+      question: "Who was the first president of the United States?",
+      options: [
+        "Thomas Jefferson",
+        "George Washington",
+        "Abraham Lincoln",
+        "John Adams",
+      ],
+      answer: 1,
     },
     {
-      question: 'Which Impel Down floor was frozen?',
-      options: ['3', '5', '4', '2'],
-      answer: 1
-    },
-    // ... Add more questions
-    {
-      question: 'Question 1',
-      options: ['Op', 'Option 2', 'Option 3', 'Option 4'],
-      answer: 0 // Index of the correct option
+      question: "Which country was the first to reach the moon?",
+      options: ["United States", "Russia", "China", "India"],
+      answer: 0,
     },
     {
-      question: 'Question 1',
-      options: ['Option 1', 'Option 2', 'Option 3', 'Option 4'],
-      answer: 0 // Index of the correct option
+      question: "Who painted the Mona Lisa?",
+      options: [
+        "Pablo Picasso",
+        "Vincent van Gogh",
+        "Leonardo da Vinci",
+        "Michelangelo",
+      ],
+      answer: 2,
     },
     {
-      question: 'Question 1',
-      options: ['Option 1', 'Option 2', 'Option 3', 'Option 4'],
-      answer: 0 // Index of the correct option
+      question: "When did World War II end?",
+      options: ["1939", "1945", "1942", "1950"],
+      answer: 1,
     },
     {
-      question: 'Question 1',
-      options: ['Option 1', 'Option 2', 'Option 3', 'Option 4'],
-      answer: 0 // Index of the correct option
+      question: "Which civilization built the Machu Picchu citadel?",
+      options: ["Maya", "Inca", "Aztec", "Egyptian"],
+      answer: 1,
     },
     {
-      question: 'Question 1',
-      options: ['Option 1', 'Option 2', 'Option 3', 'Option 4'],
-      answer: 0 // Index of the correct option
+      question: "Who is credited with discovering gravity?",
+      options: [
+        "Albert Einstein",
+        "Isaac Newton",
+        "Galileo Galilei",
+        "Charles Darwin",
+      ],
+      answer: 1,
     },
     {
-      question: 'Question 1',
-      options: ['Option 1', 'Option 2', 'Option 3', 'Option 4'],
-      answer: 0 // Index of the correct option
+      question: "In which year did the Berlin Wall fall?",
+      options: ["1989", "1961", "1975", "1991"],
+      answer: 0,
     },
     {
-      question: 'Question 1',
-      options: ['Option 1', 'Option 2', 'Option 3', 'Option 4'],
-      answer: 0 // Index of the correct option
+      question: 'Who wrote the play "Romeo and Juliet"?',
+      options: [
+        "William Shakespeare",
+        "Jane Austen",
+        "Mark Twain",
+        "George Orwell",
+      ],
+      answer: 0,
     },
     {
-      question: 'Question 1',
-      options: ['Option 1', 'Option 2', 'Option 3', 'Option 4'],
-      answer: 0 // Index of the correct option
-    }
+      question: "Which explorer is known for discovering America?",
+      options: [
+        "Christopher Columbus",
+        "Vasco da Gama",
+        "Ferdinand Magellan",
+        "James Cook",
+      ],
+      answer: 0,
+    },
+    {
+      question: "When was the Magna Carta signed?",
+      options: ["1215", "1492", "1776", "1804"],
+      answer: 0,
+    },
   ];
 
   const [currentQuestion, setCurrentQuestion] = useState(0);
   const [score, setScore] = useState(0);
   const [showScore, setShowScore] = useState(false);
-  const [userAnswers, setUserAnswers] = useState(Array(questions.length).fill(null));
+  const [userAnswers, setUserAnswers] = useState(
+    Array(questions.length).fill(null)
+  );
   const [submitted, setSubmitted] = useState(false);
 
   const handleAnswerOptionClick = (answerIndex) => {
@@ -115,21 +141,29 @@ const Quiz = () => {
             {questions.map((question, index) => (
               <div
                 key={index}
-                className={`question-feedback ${userAnswers[index] === question.answer ? 'correct' : 'incorrect'}`}
+                className={`question-feedback ${
+                  userAnswers[index] === question.answer
+                    ? "correct"
+                    : "incorrect"
+                }`}
               >
                 <p>{question.question}</p>
                 {question.options.map((option, optionIndex) => (
                   <div
                     key={optionIndex}
-                    className={`answer-feedback ${userAnswers[index] === optionIndex ? 'selected' : ''}`}
+                    className={`answer-feedback ${
+                      userAnswers[index] === optionIndex ? "selected" : ""
+                    }`}
                   >
                     {option}
-                    {userAnswers[index] === optionIndex && userAnswers[index] !== question.answer && (
-                      <span className="answer-feedback-icon">✗</span>
-                    )}
-                    {userAnswers[index] !== optionIndex && question.answer === optionIndex && (
-                      <span className="answer-feedback-icon">✓</span>
-                    )}
+                    {userAnswers[index] === optionIndex &&
+                      userAnswers[index] !== question.answer && (
+                        <span className="answer-feedback-icon">✗</span>
+                      )}
+                    {userAnswers[index] !== optionIndex &&
+                      question.answer === optionIndex && (
+                        <span className="answer-feedback-icon">✓</span>
+                      )}
                   </div>
                 ))}
               </div>
@@ -143,7 +177,9 @@ const Quiz = () => {
         <>
           <div className="question-section">
             <div className="question-count">
-              <h1>Question {currentQuestion + 1}/{questions.length}</h1>
+              <h1>
+                Question {currentQuestion + 1}/{questions.length}
+              </h1>
             </div>
             <div className="question-text">
               <h3>{questions[currentQuestion].question}</h3>
@@ -153,7 +189,9 @@ const Quiz = () => {
             {questions[currentQuestion].options.map((option, index) => (
               <button
                 key={index}
-                className={`answer-option ${userAnswers[currentQuestion] === index ? 'selected' : ''}`}
+                className={`answer-option ${
+                  userAnswers[currentQuestion] === index ? "selected" : ""
+                }`}
                 onClick={() => handleAnswerOptionClick(index)}
               >
                 {option}
@@ -168,12 +206,14 @@ const Quiz = () => {
             >
               Previous
             </button>
-            
+
             <div className="numbered-buttons">
               {questions.map((_, index) => (
                 <button
                   key={index}
-                  className={`numbered-button ${index === currentQuestion ? 'active' : ''}`}
+                  className={`numbered-button ${
+                    index === currentQuestion ? "active" : ""
+                  }`}
                   onClick={() => handleNumberClick(index)}
                 >
                   {index + 1}
@@ -188,10 +228,9 @@ const Quiz = () => {
             >
               Next
             </button>
-
-            <button className="submit-button" onClick={handleSubmitClick}>
-              Submit
-            </button>
+          </div>
+          <div className="submit-button">
+            <button onClick={handleSubmitClick}>Submit</button>
           </div>
         </>
       )}
